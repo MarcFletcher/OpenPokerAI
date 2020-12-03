bd17576778a6d6e6fde4b664ad91c9796d2e5f02
Marc Fletcher <marc@echobox.com>
2020-12-03 09:21:58 +0000

    Renamed Defintions project

--- PokerDefinitions/Properties/AssemblyInfo.cs
+++ PokerDefinitions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PokerBotDefinitions")]
+[assembly: AssemblyTitle("PokerDefinitions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("PokerBotDefinitions")]
+[assembly: AssemblyProduct("PokerDefinitions")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
